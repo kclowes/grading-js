ec9bfd8760d081099b0fd2168c0c64434e7eb1bc
Keri Clowes <kerilyn.clowes@gmail.com>
2014-06-23 15:53:44 -0600

    an alert is displayed with the difference between the array components

--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,7 +1,23 @@
 window.GradeParser = {
 
-  getMovements: function (grades) {
-    return [];
+  getMovements: function () {
+    var grades = [1, 2, 3, 8];
+    var results = [];
+    for (var i = 0; i < (grades.length - 1); i++) {
+      var diff = grades[i + 1] - grades[i];
+      if (diff > 0)
+        results[results.length] = 'up';
+      else if (diff < 0)
+        results[results.length] = 'down';
+      else if (diff = 0)
+        results[results.length] = 'even';
+    }
+    alert(results);
   }
-
 };
+
+
+$(document).ready(function () {
+  window.GradeParser.getMovements();
+});
+
