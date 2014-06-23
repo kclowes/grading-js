3763146a87a30f84c10efc6a27fa0c4323d92439
Keri Clowes <kerilyn.clowes@gmail.com>
2014-06-23 15:56:15 -0600

    Fixed numbers being even conditional

--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,7 +1,7 @@
 window.GradeParser = {
 
   getMovements: function () {
-    var grades = [1, 2, 3, 8];
+    var grades = [1, 2, 2, 1];
     var results = [];
     for (var i = 0; i < (grades.length - 1); i++) {
       var diff = grades[i + 1] - grades[i];
@@ -9,7 +9,7 @@ window.GradeParser = {
         results[results.length] = 'up';
       else if (diff < 0)
         results[results.length] = 'down';
-      else if (diff = 0)
+      else if (diff == 0)
         results[results.length] = 'even';
     }
     alert(results);
