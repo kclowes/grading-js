27d51c859b0b673205d39e0bb5f51fcdb2c8811c
Keri Clowes <kerilyn.clowes@gmail.com>
2014-06-23 17:15:39 -0600

    User can tell if a student's grade is in decline or not

--- src/grade_parser.js
+++ src/grade_parser.js
@@ -1,7 +1,7 @@
 window.GradeParser = {
 
   getMovements: function () {
-    var grades = [1, 2, 2, 1];
+    var grades = [1, 2, 4, 3, 3, 4];
     var results = [];
     for (var i = 0; i < (grades.length - 1); i++) {
       var diff = grades[i + 1] - grades[i];
@@ -12,7 +12,20 @@ window.GradeParser = {
       else if (diff == 0)
         results[results.length] = 'even';
     }
-    alert(results);
+    console.log(results);
+
+    var last_three_array = [results[results.length - 3], results[results.length - 2], results[results.length - 1]];
+    console.log(last_three_array);
+
+    var index = last_three_array.indexOf('up')
+    console.log(index)
+
+    if (results.length < 3)
+      console.log('not enough data');
+    else if (index === -1)
+      console.log('in decline');
+    else
+      console.log('not in decline');
   }
 };
 
